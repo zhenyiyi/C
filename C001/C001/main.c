3b782dd9c05fe6c0e258eb0f07048338304ed0ff
zhenyiyi <15618211029@163.com>
2017-06-15 17:59:23 +0800

    001.c

---
+++ C001/C001/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C001
+//
+//  Created by fenglin on 2017/6/15.
+//  Copyright © 2017年 yhy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
