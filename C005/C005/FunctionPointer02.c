59005d9d37d9017f01536d1cbb5960c929eefed8
pingtanglu <857979844@qq.com>
2017-06-17 17:32:24 +0800

    函数指针

--- C005/C005/FunctionPointer02.c
+++ C005/C005/FunctionPointer02.c
@@ -7,23 +7,66 @@
 //
 
 #include "FunctionPointer02.h"
+#include <stdlib.h>
 
+#pragma mark --C 语言的函数指针
 void inviteAttend(int nameCount, char **(*AttendNames)(void)){
     char **p = AttendNames();
     for (int i=0; i<nameCount; i++) {
-        printf("%s",p[i]);
+        printf("%s\n",p[i]);
     }
 }
-
+static char *arr[3] = {"fenglin","QiQi","wenhua"};
 char ** getNextRandomAttendName(){
-    static char *arr[3] = {"fenglin","QiQi","wenhua"};
+    printf("获取当前的数组-> %s",__func__);
     return arr;
 }
 
+
+
+void fillArray(int *arr, size_t arraySize, int(*getNextValue)(void)){
+    for (int i=0; i<arraySize; i++) {
+        arr[i] = getNextValue();
+    }
+}
+int getNextRandomValue(){
+    return rand();
+}
+
+
+
+
+
+typedef void(*Completion)(_Bool flag, char**);
+
+void fetchPhoneList(Completion comletion){
+    char *arr[] = {
+        "1508888888","13333333333","15888888888","18699999999"
+    };
+    comletion(0, arr);
+}
+
+
+
+
+void callback(_Bool flag, char*arr[]){
+    printf("flag-> %d\n",flag);
+    for (int i=0; i<4; i++) {
+        printf("%s\n",arr[i]);
+    }
+};
 
 void test2(){
     char *a[3] = {"fenglin","QiQi","wenhua"};
     
     printf("a[0] -> %s\n",a[0]);
-    inviteAttend(3, getNextRandomAttendName());
+    inviteAttend(3, getNextRandomAttendName);
+    
+    int arr[10];
+    fillArray(arr, 10, getNextRandomValue);
+    for (int i=0; i<10; i++) {
+        printf("%d\n",arr[i]);
+    }
+    
+    fetchPhoneList(callback);
 }
